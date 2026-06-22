4eca0eea708c71aa9a8e8555ad1f75ca5866786b
Kavulapalli Manikanta <manikant14568@gmail.com>
2026-06-22 10:56:03 +0530

    Initialize Express app with middleware and routes

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const indexRoutes = require('./routes/index');
+const healthRoutes = require('./routes/health');
+const notFound = require('./middleware/notFound');
+const errorHandler = require('./middleware/errorHandler');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false
+  })
+);
+
+app.use(morgan('combined'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRoutes);
+app.use('/health', healthRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
